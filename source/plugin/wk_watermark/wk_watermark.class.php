726bdfef658781b689ee76638921c40c20a340b4
walkskyer <walkskyer@qq.com>
2014-01-10 16:25:31 +0800

    水印插件接管forum的中原始图片的水印功能。还存在一个问题需要调试。

--- source/plugin/wk_watermark/wk_watermark.class.php
+++ source/plugin/wk_watermark/wk_watermark.class.php
@@ -23,11 +23,12 @@ class plugin_wk_watermark{
     function common(){
         global $_G;
         if(!$this->vars['wk_watermark_on']) return;
-        if (empty($_FILES) || $_GET['mod'] != 'swfupload' || $_GET['action'] != 'swfupload') return false;
+        require_once DISCUZ_ROOT.'./source/plugin/wk_watermark/source/function/function_wk.php';
+        //if (empty($_FILES) || $_GET['mod'] != 'swfupload' || $_GET['action'] != 'swfupload') return false;
         $_G['uid'] = intval($_POST['uid']);
-        if((empty($_G['uid']) && $_GET['operation'] != 'upload') || $_POST['hash'] != md5(substr(md5($_G['config']['security']['authkey']), 8).$_G['uid'])) {
+        /*if((empty($_G['uid']) && $_GET['operation'] != 'upload') || $_POST['hash'] != md5(substr(md5($_G['config']['security']['authkey']), 8).$_G['uid'])) {
             exit;
-        }
+        }*/
         if($_GET['operation'] == 'upload' && $this->vars['wk_forum']) {
             if(empty($_GET['simple'])) {
                 $_FILES['Filedata']['name'] = addslashes(diconv(urldecode($_FILES['Filedata']['name']), 'UTF-8'));
@@ -41,6 +42,8 @@ class plugin_wk_watermark{
         }elseif($_GET['operation'] == 'album' && $this->vars['wk_album']) {
             require_once DISCUZ_ROOT.'./source/plugin/wk_watermark/wk_album_upload.class.php';
             wk_album_upload();
+        }elseif($_GET['mod'] == 'post'){
+            require_once DISCUZ_ROOT.'./source/plugin/wk_watermark/source/module/forum/forum_post.php';
         }
     }
 }
