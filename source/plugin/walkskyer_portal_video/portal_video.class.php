46f5b8a7b1e5383f9667a5bfe1d9ed1328bc3dc0
walkskyer <walkskyer@qq.com>
2014-01-20 17:41:56 +0800

    添加百度地图插件。

--- source/plugin/walkskyer_portal_video/portal_video.class.php
+++ source/plugin/walkskyer_portal_video/portal_video.class.php
@@ -22,6 +22,24 @@ class plugin_walkskyer_portal_video_portal extends plugin_walkskyer_portal_video
         require_once libfile('portal/'.$_GET['mod'], WS_FOLDER.'/module');
     }
     function portalcp_extend(){
-        return 'hello';
+        return '<a href="javascript:void(0);" id="ws_hello">hello</a>';
+    }
+    function portalcp_bottom(){
+        $script=<<<EOF
+        <script src="http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.7.2.js" type="text/javascript"></script>
+<script type="text/javascript">
+var j = jQuery.noConflict();
+j(document).ready(function(){
+    j("a#ws_hello").click(function(){
+        var htmlEditor = j("#HtmlEditor").contents().find("body");
+        var html=j("body", window.frames["HtmlEditor"].document).html();
+        var a=1;
+        var s=htmlEditor.text();
+        var b=2;
+    });
+});
+</script>
+EOF;
+        return $script.'<a>hello</a>';
     }
 }
