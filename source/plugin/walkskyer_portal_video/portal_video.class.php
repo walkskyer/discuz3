89061a90dbb6503830a8b6e759fde8938e35de14
walkskyer <walkskyer@qq.com>
2014-01-23 17:11:21 +0800

    门户视频插件视频上传功能复制完毕。

--- source/plugin/walkskyer_portal_video/portal_video.class.php
+++ source/plugin/walkskyer_portal_video/portal_video.class.php
@@ -22,35 +22,41 @@ class plugin_walkskyer_portal_video_portal extends plugin_walkskyer_portal_video
         require_once libfile('portal/'.$_GET['mod'], WS_FOLDER.'/module');
     }
     function portalcp_extend(){
-        return '<a href="javascript:void(0);" id="ws_hello">hello</a>';
+        return '<a href="javascript:void(0);" id="ws_video">上传视频</a>';
     }
     function portalcp_bottom(){
+        global $_G;
+        require_once libfile('function/upload');
+        $swfconfig = getuploadconfig($_G['uid'], 0, false);
+        $aid=$_GET['aid'];
+        $catid=$_GET['catid'];
         $script=<<<EOF
         <script src="http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.7.2.js" type="text/javascript"></script>
 <script type="text/javascript">
 var j = jQuery.noConflict();
-function createAttachBox(fn) {
+function createVideoBox(fn) {
 	if(typeof fn == 'function' && !fn()) {
 		return false;
 	}
-	var menu = $('icoAttach_attach_menu');
+	var menu = $('ws_icoAttach_attach_menu');
 	if(menu) {
 		if(menu.style.visibility == 'hidden') {
 			menu.style.visibility = 'visible';
 		} else {
 			menu.style.width = '600px';
-			showMenu({'ctrlid':'icoAttach_attach','mtype':'win','evt':'click','pos':'00','timeout':250,'duration':3,'drag':'icoAttach_attach_ctrl'});
+			showMenu({'ctrlid':'ws_icoAttach_attach','mtype':'win','evt':'click','pos':'00','timeout':250,'duration':3,'drag':'ws_icoAttach_attach_ctrl'});
 		}
 	}
 }
 j(document).ready(function(){
-    j("a#ws_hello").click(function(){
-        var strimg1='hello';
-        edit_insert(strimg1);
+    j("a#ws_video").click(function(){
+        createVideoBox(check_catid);
     });
 });
 </script>
 EOF;
-        return $script;
+        $return='';
+        include template('walkskyer_portal_video:editor_image_menu');
+        return $script.$return;
     }
 }
