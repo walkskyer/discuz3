22cd2d91028bb5f64cd0c64bc56c0ee0989d489e
walkskyer <walkskyer@qq.com>
2014-03-19 16:42:17 +0800

    修改投稿插件编辑器，可选择相册图片。

--- source/plugin/walkskyer_push_article/walkskyer_push_article.inc.php
+++ source/plugin/walkskyer_push_article/walkskyer_push_article.inc.php
@@ -8,4 +8,8 @@
 define('MYS','walkskyer_push_article');
 require DISCUZ_ROOT.'./source/function/function_home.php';
 require DISCUZ_ROOT.'./source/function/function_portal.php';
+require_once libfile('function/upload');
+$swfconfig = getuploadconfig($_G['uid'], 0, false);
+require_once libfile('function/spacecp');
+$albums = getalbums($_G['uid']);
 @include template(MYS.':portalcp_article');
